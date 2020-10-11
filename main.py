d3a48af7c9b39bbb1dc681ba54c50aca96d1f356
panel14 <71323711+panel14@users.noreply.github.com>
2020-10-11 20:36:19 +0300

    Исправлены недочёты

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import re
 
-with open ("steam_description_data.csv", encoding='utf-8') as file:
+with open ('steam_description_data.csv', encoding='utf-8') as file:
     str = file.read()
     reg_for_chars = re.compile('\w')
-    print("Количество символов в файле:", len(reg_for_chars.findall(str)))
     reg_for_words = re.compile('\w[\s.,!?_-]')
-    print("Количество слов в файле:", len(reg_for_words.findall(str)))
-    reg_for_sent = re.compile('\w[.,!?_-]')
-    print("Количество слов в файле:", len(reg_for_sent.findall(str)))
+    reg_for_sent = re.compile('\w[.!?]')
+    print('Количество символов в файле:', len(reg_for_chars.findall(str)),
+          '\nКоличество слов в файле:', len(reg_for_words.findall(str)),
+          '\nКоличество предложений в файле:', len(reg_for_sent.findall(str)))
